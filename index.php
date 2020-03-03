0b5bcc6e0a64ab5dca9e08217bc0978be145049c
buddi07-mc <61216370+buddi07-mc@users.noreply.github.com>
2020-03-02 23:40:01 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+include('core/init.inc.php');
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MC Friends</title>
+		<link rel="stylesheet" type="text/css" href="ext/css/main.css" />
+		<meta charset="UTF-8" />
+	</head>
+	<body>
+		<div id="wrap">
+		<?php
+		
+		include($include_file);
+		
+		?>
+	</body>
+</html>
